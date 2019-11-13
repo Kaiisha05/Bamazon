85e7905ae14fce56425794f88674f2f278ebb201
Kaiisha05 <Kaiisha@yahoo.com>
2019-11-13 14:56:49 -0500

    added mysql connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,26 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+// create connection for the sql database
+var connection = mysql.createConnection({
+host: "localhost",
+// everyone uses port 3000...
+port: 3003,
+
+// username...
+password: "docker",
+database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+shop();
+});
+
+function shop() {
+inquirer.prompt({
+
+})
+
+}
