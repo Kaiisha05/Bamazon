b61e66aeac32d1fdf45310db024c2a7b430fa487
Kaiisha05 <Kaiisha@yahoo.com>
2019-11-23 12:38:40 -0500

    added cli-table as a dependency and included prompt for questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,26 +1,38 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 // create connection for the sql database
 var connection = mysql.createConnection({
-host: "localhost",
-// everyone uses port 3000...
-port: 3003,
+    host: "localhost",
+    // everyone uses port 3000...
+    port: 3003,
 
-// username...
-password: "docker",
-database: "bamazonDB"
+    // username...
+    password: "password",
+    database: "bamazon_DB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-shop();
+
+
+    shop();
 });
 
 function shop() {
-inquirer.prompt({
-
-})
-
+    inquirer.prompt({
+        type:"confirm",
+        name:"continue",
+        message:"Hi, how may I help you?"
+    })
+    .then(response => {
+        if (response.continue) {
+            console.log("Let me  verify our inventory");
+            showInventory();
+        } else {
+            endTransaction();
+        }
+    });
 }
